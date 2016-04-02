38f33abf866386ec91126f3d55aad5f97bbeab39
An Li <kidylee@gmail.com>
2016-04-01 21:02:39 -0400

    remove counter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+
 import { App } from './App';
 
+import configureStore from './store/configureStore'
 
-render(<App />, document.getElementById('root'));
+const store = configureStore({})
+
+const Root = () => 
+	(<Provider store={store} >
+		<App />
+	</Provider>)
+
+
+
+render(
+	<Root/>
+	, document.getElementById('root'));
